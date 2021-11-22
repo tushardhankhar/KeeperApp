cf2b3862f945375f359317b59d9e5b4e080c2113
tushardhankhar <tushardhankhar98@gmail.com>
2021-11-22 13:34:17 +0530

    Notes added

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "./notes";
+
+function createNote(notes){
+    return(
+        <Note
+            key={notes.id}
+            title={notes.title}
+            content={notes.content} />
+    )
+}
 
 function App(){
     return (
         <div>
             <Header />
-            <Note></Note>
+            {notes.map(createNote)}
             <Footer />
 
         </div>
